e8afae194ea094ecafb82628e36ece39029ccfad
Vladislav Tretyakov <ididnothing86@gmail.com>
2022-08-30 03:21:48 +0600

    initial

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+        public static void main(String[] args) {
+
+            int expenses = 1575; // всего потраченных у.е.
+            int onePoint = 20; // колличество потраченных у.е. для полученния 1 балла
+
+            System.out.println(expenses / onePoint);
+
+        }
+    }
